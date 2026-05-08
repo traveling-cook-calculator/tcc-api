a1074b3e47f6667556511e61da3590cfcededb73
Bean900 <info@beancode.de>
2026-05-08 19:22:33 +0200

    try to log cicd 500 error

--- src/main.rs
+++ src/main.rs
@@ -58,7 +58,7 @@ struct AppState {
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt()
-        .with_max_level(tracing::Level::INFO)
+        .with_max_level(tracing::Level::DEBUG)
         .init();
     info!("Loading environment variables...");
 
