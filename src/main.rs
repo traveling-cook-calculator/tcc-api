3f894864141415762b37288cc8484056ce65deb8
Bean900 <info@beancode.de>
2026-05-11 18:58:35 +0200

    remove converage tests

--- src/main.rs
+++ src/main.rs
@@ -58,7 +58,7 @@ struct AppState {
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt()
-        .with_max_level(tracing::Level::DEBUG)
+        .with_max_level(tracing::Level::INFO)
         .init();
     info!("Loading environment variables...");
 
