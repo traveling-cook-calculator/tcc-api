cdf2e252f24df2af7cf916c98a68cb713ddb9195
Bean900 <info@beancode.de>
2026-05-30 20:44:12 +0200

    test new pattern

--- tests/api/team/note/get_test.rs
+++ tests/api/team/note/get_test.rs
@@ -214,7 +214,7 @@ fn assert_note_json(json: &serde_json::Value, expected_note_id: &Uuid) {
     );
 
     assert!(
-        NaiveDateTime::parse_from_str(created, "%Y-%m-%dT%H:%M:00").is_ok(),
+        NaiveDateTime::parse_from_str(created, "%Y-%m-%dT%H:%M:00Z").is_ok(),
         "Created is not a valid NaiveTime: {}",
         created
     );
